84bf34ef838b2e4bcdedfffa18a67780f101803b
Jason Luc <j.luc2012@gmail.com>
2013-08-14 11:49:01 -0400

    Update all 'variables' that have variable type 'integer' or 'numeric' to have 'units' in VALID_UNITS and update 'dictionary_test' to reflect the correct VALID_UNITS

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -3,20 +3,24 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
-  # VALID_UNITS = ['minutes', 'hours']
+  VALID_UNITS = ['minutes', 'hours', 'mm H2O', 'units', 'nights', 'cm H2O', 'MET',
+    'unit count', 'years old', 'event count', 'mm', 'cm', 'ft', 'lbs', 'in', 'kg/m2',
+    'm/s', 'cm/sec', 'g', 'g/m2', 'ml', 'percent', 'mmHg', 'mg/L', 'mg/dl', 'ug/mL',
+    'cm2', 'ml/m2', 'Wood', 'days', 'beats per minute', 'kg', 'years', 'events per hour',
+     '']
 
-  # def assert_units(units, msg = nil)
-  #   full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
-  #   assert_block(full_message) do
-  #     VALID_UNITS.include?(units)
-  #   end
-  # end
+  def assert_units(units, msg = nil)
+    full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
+    assert_block(full_message) do
+      VALID_UNITS.include?(units)
+    end
+  end
 
-  # Dir.glob("variables/**/*.json").each do |file|
-  #   if ['numeric','integer'].include?(json_value(file, :type))
-  #     define_method("test_units: "+file) do
-  #       assert_units json_value(file, :units)
-  #     end
-  #   end
-  # end
+  Dir.glob("variables/**/*.json").each do |file|
+    if ['numeric','integer'].include?(json_value(file, :type))
+      define_method("test_units: "+file) do
+        assert_units json_value(file, :units)
+      end
+    end
+  end
 end
